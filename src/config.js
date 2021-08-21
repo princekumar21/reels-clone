9036a7623ab685d37eedc1649ab305addab3dba4
princekumar21 <pk41330@gmail.com>
2021-08-21 21:01:51 +0530

    update

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDeTUo-piohcgID_V8vJYo36wL-sQutMo4",
-  authDomain: "reels-facb5.firebaseapp.com",
-  projectId: "reels-facb5",
-  storageBucket: "reels-facb5.appspot.com",
-  messagingSenderId: "600794493976",
-  appId: "1:600794493976:web:b982972a964b43a3d73f6d",
-};
